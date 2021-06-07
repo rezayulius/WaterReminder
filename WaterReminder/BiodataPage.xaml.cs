b01fbb2526587573476c9e3d5ad1dfed3aaae27f
rezafanfanayulius12@gmail.com <rezafanfanayulius12@gmail.com>
2021-06-07 13:53:29 +0700

    BiodataPage

---
+++ WaterReminder/BiodataPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WaterReminder
+{
+    /// <summary>
+    /// Interaction logic for BiodataPage.xaml
+    /// </summary>
+    public partial class BiodataPage : Window
+    {
+        public BiodataPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
